f3360516b460a53da5ca5b3e86caf72ad1336b5d
yxy <ltyxy@buaa.edu.cn>
2024-03-19 00:35:26 +0800

    add clang format config and format all files

--- gemm.cu
+++ gemm.cu
@@ -11,89 +11,88 @@
 // B : k * n
 
 __global__ void gemm(float *A, float *B, float *C, int m, int n, int k) {
-  int col = blockDim.y * blockIdx.y + threadIdx.y;
-  int row = blockDim.x * blockIdx.x + threadIdx.x;
-  float sum = 0;
-  for (int i = 0; i < k; i++) {
-    sum += A[row * k + i] * B[i * n + col];
-  }
-  C[row * n + col] = sum;
+    int col = blockDim.y * blockIdx.y + threadIdx.y;
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+    float sum = 0;
+    for(int i = 0; i < k; i++) {
+        sum += A[row * k + i] * B[i * n + col];
+    }
+    C[row * n + col] = sum;
 }
 
 __global__ void gemm_shm(float *A, float *B, float *C, int m, int n, int k) {
-  __shared__ float share_A[TILE_SIZE][TILE_SIZE];
-  __shared__ float share_B[TILE_SIZE][TILE_SIZE];
+    __shared__ float share_A[TILE_SIZE][TILE_SIZE];
+    __shared__ float share_B[TILE_SIZE][TILE_SIZE];
 
-  int col = blockDim.y * blockIdx.y + threadIdx.y;
-  int row = blockDim.x * blockIdx.x + threadIdx.x;
+    int col = blockDim.y * blockIdx.y + threadIdx.y;
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
 
-  float sum = 0;
+    float sum = 0;
 
-  // load A to shared mem
-  for (int tile_k = 0; tile_k < k; tile_k += TILE_SIZE) {
-    share_A[threadIdx.y][threadIdx.x] = A[row * k + (threadIdx.y + tile_k)];
-    share_B[threadIdx.y][threadIdx.x] = B[(tile_k + threadIdx.x) * n + col];
-    __syncthreads();
-    float inner_sum = 0;
-    for (int inner_k = 0; inner_k < TILE_SIZE; inner_k++) {
-      inner_sum +=
-          share_A[threadIdx.y][inner_k] * share_B[inner_k][threadIdx.x];
+    // load A to shared mem
+    for(int tile_k = 0; tile_k < k; tile_k += TILE_SIZE) {
+        share_A[threadIdx.y][threadIdx.x] = A[row * k + (threadIdx.y + tile_k)];
+        share_B[threadIdx.y][threadIdx.x] = B[(tile_k + threadIdx.x) * n + col];
+        __syncthreads();
+        float inner_sum = 0;
+        for(int inner_k = 0; inner_k < TILE_SIZE; inner_k++) {
+            inner_sum += share_A[threadIdx.y][inner_k] * share_B[inner_k][threadIdx.x];
+        }
+        sum += inner_sum;
+        __syncthreads();
     }
-    sum += inner_sum;
-    __syncthreads();
-  }
-  C[row * n + col] = sum;
+    C[row * n + col] = sum;
 }
 
 void printMatrix(float *A, int m, int n) {
-  for (int i = 0; i < m; i++) {
-    printf("[");
-    for (int j = 0; j < n; j++) {
-      printf("%f ", A[i * n + j]);
+    for(int i = 0; i < m; i++) {
+        printf("[");
+        for(int j = 0; j < n; j++) {
+            printf("%f ", A[i * n + j]);
+        }
+        printf("]\n");
     }
-    printf("]\n");
-  }
 }
 
 int main() {
-  float *A, *B, *C;
-  float *d_A, *d_B, *d_C;
-  int m = 1024, n = 1024, k = 1024;
-  int size_A = m * k * sizeof(float);
-  int size_B = k * n * sizeof(float);
-  int size_C = m * n * sizeof(float);
+    float *A, *B, *C;
+    float *d_A, *d_B, *d_C;
+    int m = 1024, n = 1024, k = 1024;
+    int size_A = m * k * sizeof(float);
+    int size_B = k * n * sizeof(float);
+    int size_C = m * n * sizeof(float);
 
-  A = (float *)malloc(size_A);
-  B = (float *)malloc(size_B);
-  C = (float *)malloc(size_C);
+    A = (float *)malloc(size_A);
+    B = (float *)malloc(size_B);
+    C = (float *)malloc(size_C);
 
-  cudaMalloc(&d_A, size_A);
-  cudaMalloc(&d_B, size_B);
-  cudaMalloc(&d_C, size_C);
+    cudaMalloc(&d_A, size_A);
+    cudaMalloc(&d_B, size_B);
+    cudaMalloc(&d_C, size_C);
 
-  for (int i = 0; i < m * k; i++) {
-    A[i] = 1.0;
-  }
+    for(int i = 0; i < m * k; i++) {
+        A[i] = 1.0;
+    }
 
-  for (int i = 0; i < k * n; i++) {
-    B[i] = 1.0;
-  }
+    for(int i = 0; i < k * n; i++) {
+        B[i] = 1.0;
+    }
 
-  for (int i = 0; i < m * n; i++) {
-    C[i] = 0.0;
-  }
+    for(int i = 0; i < m * n; i++) {
+        C[i] = 0.0;
+    }
 
-  cudaMemcpy(d_A, A, size_A, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, B, size_B, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_C, C, size_C, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, size_A, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size_B, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_C, C, size_C, cudaMemcpyHostToDevice);
 
-  dim3 grid(m / 16, n / 16);
-  dim3 block(16, 16);
+    dim3 grid(m / 16, n / 16);
+    dim3 block(16, 16);
 
-  gemm<<<grid, block>>>(d_A, d_B, d_C, m, n, k);
+    gemm<<<grid, block>>>(d_A, d_B, d_C, m, n, k);
 
-  cudaMemcpy(C, d_C, size_C, cudaMemcpyDeviceToHost);
+    cudaMemcpy(C, d_C, size_C, cudaMemcpyDeviceToHost);
 
-  // printMatrix(C, m, n);
-  return 0;
+    // printMatrix(C, m, n);
+    return 0;
 }
--- transpose.cu
+++ transpose.cu
@@ -6,113 +6,150 @@ static constexpr int TILESIZE = 32;
 static constexpr int ROWSIZE = 8;
 
 void printMatrix(float *A, int m, int n) {
-  for (int i = 0; i < m; i++) {
-    printf("[");
-    for (int j = 0; j < n; j++) {
-      printf("%f ", A[i * n + j]);
+    for(int i = 0; i < m; i++) {
+        printf("[");
+        for(int j = 0; j < n; j++) {
+            printf("%f ", A[i * n + j]);
+        }
+        printf("]\n");
     }
-    printf("]\n");
-  }
 }
 
 bool check(float *A, float *B, int size, float eps = 1e-2) {
-  for (int i = 0; i < size; i++) {
-    if (abs(A[i] - B[i]) > eps) {
-      printf("A[%d] = %f, B[%d] = %f\n", i, A[i], i, B[i]);
-      return false;
+    for(int i = 0; i < size; i++) {
+        if(abs(A[i] - B[i]) > eps) {
+            printf("A[%d] = %f, B[%d] = %f\n", i, A[i], i, B[i]);
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 __global__ void transpose(float *in, float *out, int m, int n) {
-  int row = blockDim.y * blockIdx.y + threadIdx.y;
-  int col = blockDim.x * blockIdx.x + threadIdx.x;
+    int row = blockDim.y * blockIdx.y + threadIdx.y;
+    int col = blockDim.x * blockIdx.x + threadIdx.x;
 
-  if (row < m && col < n)
-    out[col * m + row] = in[row * n + col];
+    if(row < m && col < n)
+        out[col * m + row] = in[row * n + col];
 }
 
-template <int TILE_SIZE,
-          int ROW_SIZE> // blockDim.x == TILE_SIZE, blockDim.y == ROW_SIZE
+template<
+    int TILE_SIZE,
+    int ROW_SIZE> // blockDim.x == TILE_SIZE, blockDim.y == ROW_SIZE
 __global__ void transpose_shared(float *in, float *out, int m, int n) {
-  int row = blockIdx.y * TILE_SIZE + threadIdx.y;
-  int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
 
-  __shared__ float tile[TILE_SIZE][TILE_SIZE + 1];
+    __shared__ float tile[TILE_SIZE][TILE_SIZE + 1];
 
-  for (int i = 0; i < TILE_SIZE; i += blockDim.y) {
-    if (row + i < m && col < n) {
-      tile[i + threadIdx.y][threadIdx.x] = in[(row + i) * n + col];
+    for(int i = 0; i < TILE_SIZE; i += blockDim.y) {
+        if(row + i < m && col < n) {
+            tile[i + threadIdx.y][threadIdx.x] = in[(row + i) * n + col];
+        }
     }
-  }
 
-  __syncthreads();
+    __syncthreads();
 
-  int out_row = blockIdx.x * TILE_SIZE + threadIdx.y;
-  int out_col = blockIdx.y * TILE_SIZE + threadIdx.x;
+    int out_row = blockIdx.x * TILE_SIZE + threadIdx.y;
+    int out_col = blockIdx.y * TILE_SIZE + threadIdx.x;
 
-  for (int i = 0; i < TILE_SIZE; i += blockDim.y) {
-    out[(out_row + i) * m + out_col] = tile[threadIdx.x][i + threadIdx.y];
-  }
+    for(int i = 0; i < TILE_SIZE; i += blockDim.y) {
+        out[(out_row + i) * m + out_col] = tile[threadIdx.x][i + threadIdx.y];
+    }
+}
+
+template<
+    int TILE_SIZE,
+    int ROW_SIZE> // blockDim.x == TILE_SIZE, blockDim.y == ROW_SIZE
+__global__ void transpose_shared_bankconflict(float *in, float *out, int m, int n) {
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+
+    __shared__ float tile[TILE_SIZE][TILE_SIZE];
+
+    for(int i = 0; i < TILE_SIZE; i += blockDim.y) {
+        if(row + i < m && col < n) {
+            tile[i + threadIdx.y][threadIdx.x] = in[(row + i) * n + col];
+        }
+    }
+
+    __syncthreads();
+
+    int out_row = blockIdx.x * TILE_SIZE + threadIdx.y;
+    int out_col = blockIdx.y * TILE_SIZE + threadIdx.x;
+
+    for(int i = 0; i < TILE_SIZE; i += blockDim.y) {
+        out[(out_row + i) * m + out_col] = tile[threadIdx.x][i + threadIdx.y];
+    }
 }
 
 int main() {
-  float *in;
-  float *out;
-  float *out_golden;
-  float *d_in;
-  float *d_out;
-  int m = 32;
-  int n = 32;
+    float *in;
+    float *out;
+    float *out_golden;
+    float *d_in;
+    float *d_out;
+    int m = 32;
+    int n = 32;
 
-  int size_in = m * n * sizeof(float);
-  int size_out = m * n * sizeof(float);
+    int size_in = m * n * sizeof(float);
+    int size_out = m * n * sizeof(float);
 
-  in = (float *)malloc(size_in);
-  out = (float *)malloc(size_out);
-  out_golden = (float *)malloc(size_out);
+    in = (float *)malloc(size_in);
+    out = (float *)malloc(size_out);
+    out_golden = (float *)malloc(size_out);
 
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      in[i * n + j] = i;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            in[i * n + j] = i;
+        }
     }
-  }
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      out_golden[i * m + j] = in[j * n + i];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            out_golden[i * m + j] = in[j * n + i];
+        }
     }
-  }
 
-  cudaMalloc((void **)&d_in, size_in);
-  cudaMalloc((void **)&d_out, size_out);
+    cudaMalloc((void **)&d_in, size_in);
+    cudaMalloc((void **)&d_out, size_out);
 
-  cudaMemcpy(d_in, in, size_in, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_in, in, size_in, cudaMemcpyHostToDevice);
 
-  dim3 dimGrid((m + 15) / 16, (n + 15) / 16);
-  dim3 dimBlock(16, 16);
+    dim3 dimGrid((m + 15) / 16, (n + 15) / 16);
+    dim3 dimBlock(16, 16);
 
-  transpose<<<dimGrid, dimBlock>>>(d_in, d_out, m, n);
+    transpose<<<dimGrid, dimBlock>>>(d_in, d_out, m, n);
 
-  // dim3 dimGrid_shared((m + TILESIZE - 1) / TILESIZE,
-  //                     (n + TILESIZE - 1) / TILESIZE);
-  // dim3 dimBlock_shared(TILESIZE, ROWSIZE);
-  // transpose_shared<TILESIZE, ROWSIZE><<<dimGrid_shared, dimBlock_shared>>>(d_in, d_out, m, n);
+    dim3 dimGrid_shared((m + TILESIZE - 1) / TILESIZE, (n + TILESIZE - 1) / TILESIZE);
+    dim3 dimBlock_shared(TILESIZE, ROWSIZE);
+    // time the kernel
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
 
-  cudaMemcpy(out, d_out, size_out, cudaMemcpyDeviceToHost);
+    transpose_shared<TILESIZE, ROWSIZE><<<dimGrid_shared, dimBlock_shared>>>(d_in, d_out, m, n);
 
-  printf("Output:\n");
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("Time for the kernel: %f ms\n", milliseconds);
 
-  printMatrix(out, n, m);
+    cudaMemcpy(out, d_out, size_out, cudaMemcpyDeviceToHost);
 
-  printf("\nGolden:\n");
+    printf("Output:\n");
 
-  printMatrix(out_golden, n, m);
+    printMatrix(out, n, m);
 
-  if (check(out, out_golden, m * n)) {
-    printf("Success!\n");
-  } else {
-    printf("Failed!\n");
-  }
+    printf("\nGolden:\n");
+
+    printMatrix(out_golden, n, m);
+
+    if(check(out, out_golden, m * n)) {
+        printf("Success!\n");
+    } else {
+        printf("Failed!\n");
+    }
 }
