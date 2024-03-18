0e3d9ac59ce38338334a94b144b2156142b214d6
yxy <ltyxy@buaa.edu.cn>
2024-03-19 00:44:21 +0800

    modify reduce

--- reduce_sum.cu
+++ reduce_sum.cu
@@ -36,12 +36,10 @@ __global__ void kernel2(float *arr, float *out, int N) {
     __syncthreads();
 
     for(int s = 1; s < blockDim.x; s *= 2) {
-        int index
-            = tid * 2
-              * s; // 原来是每个线程对应一个位置，第一轮循环，只有0、2、4、6这些线程在执行，1、3、5线程闲置，同一个warp内有一半线程没有用上
-        if((index + s) < blockDim.x
-           && (blockIdx.x * blockDim.x + index + s)
-                  < N) { // 现在是tid号线程处理处理tid*2*s位置的任务，第一轮循环0123456线程都在线，warp利用率高
+        int index = tid * 2 * s;
+        // 原来是每个线程对应一个位置，第一轮循环，只有0、2、4、6这些线程在执行，1、3、5线程闲置，同一个warp内有一半线程没有用上
+        if((index + s) < blockDim.x && (blockIdx.x * blockDim.x + index + s) < N) {
+            // 现在是tid号线程处理处理tid*2*s位置的任务，第一轮循环0123456线程都在线，warp利用率高
             s_data[index] += s_data[index + s];
         }
         __syncthreads();
@@ -61,8 +59,8 @@ __global__ void kernel3(float *arr, float *out, int N) {
     }
     __syncthreads();
 
-    for(int s = blockDim.x / 2; s > 0;
-        s >>= 1) { // kernel2的访问share memory的方式，存在share memory bank conflit
+    for(int s = blockDim.x / 2; s > 0; s >>= 1) {
+        // kernel2的访问share memory的方式，存在share memory bank conflit
         if(tid < s && i + s < N) {
             s_data[tid] += s_data[tid + s];
         }
